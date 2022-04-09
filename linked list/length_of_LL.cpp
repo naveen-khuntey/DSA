3adefdcd202188e884d82caacb06f7baa976f664
naveen khuntey <naveenkhuntek@gmail.com>
2022-04-09 12:17:59 +0530

    code2

---
+++ linked list/length_of_LL.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+/*length function*/
+int length(struct Node *P){
+    int cnt=0;
+    while(P!=NULL){
+        cnt++;
+        P=P->next;
+    }
+    return cnt;
+}
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+//reverse display
+void RDisplay(struct Node *p)
+{
+    if (p != NULL)
+    {
+        RDisplay(p->next);
+        cout<<p->data<<" ";
+    }
+}
+int main()
+{
+    struct Node *temp;
+    int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
+    create(A, 8);
+    Display(first);
+    int cnt = length(first);
+    cout<<endl<<cnt;
+    return 0;
+}
